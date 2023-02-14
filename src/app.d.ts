cd35b1028904d2ad5aa2c137856b0a46bfe42c9c
Aqil <aqilll083@gmail.com>
2023-02-14 14:40:32 +0400

    chore: add prisma & initialize it

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,5 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+import type { PrismaClient } from '@prisma/client';
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -7,6 +7,7 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
+	let prisma: PrismaClient;
 }
 
 export {};
