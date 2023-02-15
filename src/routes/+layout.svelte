e1082ab5521246b1ee28fa09772bf03bd2aa88e2
Aqil <aqilll083@gmail.com>
2023-02-15 06:46:06 +0400

    chore: add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<div>
+	<nav>
+		<ul>
+			<li>
+				<strong>
+					<a href="/"> Feel-Free </a>
+				</strong>
+			</li>
+		</ul>
+		<ul>
+			<form method="POST">
+				<li><a href="/">Home</a></li>
+				{#if !data.user}
+					<li><a href="/register">Register</a></li>
+					<li><a href="/login" role="button">Login</a></li>
+				{:else}
+					<li><button formaction="/logout" type="submit"> Logout</button></li>
+				{/if}
+			</form>
+		</ul>
+	</nav>
+	<slot />
+</div>
