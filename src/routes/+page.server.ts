ceb04358c5a696fc830e7d0f12d178dab51e9268
Aqil <aqilll083@gmail.com>
2023-02-17 17:24:27 +0400

    chore: implement listing articles

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,13 @@
+import type { PageServerLoad } from './$types';
+import { prisma } from '$lib/server/prisma';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const { user, session } = await locals.validateUser();
+	return {
+		articles: await prisma.article.findMany({
+			where: {
+				userId: user?.userId
+			}
+		})
+	};
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: ({ articles } = data);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#if data.user}
+	<h2>Articles:</h2>
+	{#each articles as aritcle}
+		<p>{aritcle.content}</p>
+	{/each}
+{/if}
