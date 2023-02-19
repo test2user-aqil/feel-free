f2d4bb1cbc6dbe88c08ec13e7364593d4419d3a2
Aqil <aqilll083@gmail.com>
2023-02-19 14:37:28 +0400

    chore: add style to navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,23 +6,22 @@
 </script>
 
 <div>
-	<nav>
+	<nav
+		class="w-full h-14 flex justify-between items-center p-3 md:px-14 bg-neutral-900 text-slate-200 border-b-[1px] border-b-indigo-500"
+	>
+		<h1>
+			<a href="/" class="font-semibold"> Feel-Free </a>
+		</h1>
 		<ul>
-			<li>
-				<strong>
-					<a href="/"> Feel-Free </a>
-				</strong>
-			</li>
-		</ul>
-		<ul>
-			<form method="POST">
-				<li><a href="/">Home</a></li>
+			<form method="POST" class="flex flex-row md:gap-4 gap-2">
 				{#if !data.user}
-					<li><a href="/register">Register</a></li>
 					<li><a href="/login" role="button">Login</a></li>
+					<li><a href="/register" class="btn-primary">Register</a></li>
 				{:else}
 					<li><button formaction="/logout" type="submit"> Logout</button></li>
-					<li><a href="/new">Create new</a></li>
+					<li>
+						<a href="/new" class="btn-primary">Create New</a>
+					</li>
 				{/if}
 			</form>
 		</ul>
