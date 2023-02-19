67796d04193788fac71195986110730cff17dce2
Aqil <aqilll083@gmail.com>
2023-02-19 14:59:32 +0400

    feat: make articles seen by everyone User can see the articles without authentication

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-{#if data.user}
-	<h2>Articles:</h2>
-	{#each articles as aritcle}
-		<p>{aritcle.content}</p>
-	{/each}
-{/if}
+<h2>Latest articles:</h2>
+{#each articles as aritcle}
+	<p>{aritcle.content}</p>
+{/each}
