be77dbe005736daaaa0c03be0e8036fde017a43a
Aqil <aqilll083@gmail.com>
2023-02-23 20:47:58 +0400

    feat: add relative date

--- src/lib/components/ArticleCard.svelte
+++ src/lib/components/ArticleCard.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
+	import { formatRelative } from 'date-fns';
 	export let id: string;
 	export let title: string;
 	export let description: string;
 	export let date: Date;
 	export let author: string;
 	export let username: string;
+
+	let formattedDate = formatRelative(date, new Date());
+	formattedDate = formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
 </script>
 
 <div class="bg-black flex flex-col justify-between rounded-xl shadow-lg border border-white/5">
-	<div class="px-4 py-3">
-		<a href={`/a/${id}`} class="font-semibold text-lg"> {title} </a>
+	<div class="p-4">
+		<a href={`/a/${id}`} class="font-semibold text-2xl hover:text-sky-200"> {title} </a>
 	</div>
 
 	<p class="py-2 px-4 text-sm">
 		{description ? description : ''}
 	</p>
 
-	<div class="border-t border-white/20 py-2 px-4 flex justify-between">
-		<p>{date.toLocaleString()}</p>
-		<a href={`/u/${username}`}>{author}</a>
+	<div class="border-t border-white/20 py-2 px-4 flex justify-between items-center">
+		<p class="text-sm">{formattedDate}</p>
+		<a href={`/u/${username}`} class="font-semibold underline hover:text-sky-300">{author}</a>
 	</div>
 </div>
