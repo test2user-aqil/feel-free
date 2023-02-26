1cc44403bdf9fead976b84e848d91320f2f8f471
Aqil <aqilll083@gmail.com>
2023-02-26 09:45:01 +0400

    feat: add style to register page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,18 +1,33 @@
-<form method="POST">
+<form method="POST" class="max-w-md mx-auto grid gap-4 py-10">
 	<hgroup>
-		<h2>Register</h2>
-		<h3>To post articles, you'll need an account.</h3>
+		<h2 class="font-medium text-xl text-center">Register</h2>
+		<h3 class="text-sm text-center font-bold">To post articles, you'll need an account.</h3>
 	</hgroup>
 
-	<label for="name">Name</label>
-	<input type="text" id="name" name="name" required />
+	<div class="grid">
+		<label for="name">Name</label>
+		<input type="text" id="name" name="name" class="input" required />
+	</div>
 
-	<label for="username">Username</label>
-	<input type="text" id="username" name="username" required />
+	<div class="grid">
+		<label for="username">Username</label>
+		<input type="text" id="username" name="username" class="input" required />
+	</div>
 
-	<label for="password">Password</label>
-	<input type="password" id="password" name="password" required />
+	<div class="grid">
+		<label for="password">Password</label>
+		<input type="password" id="password" name="password" class="input" required />
+	</div>
 
-	<button type="submit">Register</button>
+	<button type="submit" class="btn-primary">Register</button>
+
+	<p class="text-xs text-center">
+		Already have an account? <a href="/login" class="link">Login</a>
+	</p>
 </form>
-<p>Already have an account? <a href="/login">Login</a></p>
+
+<style>
+	label {
+		@apply text-xs pl-1;
+	}
+</style>
