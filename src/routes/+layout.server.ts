73bbfa787fad9f9a69706bc0622bff25689408b0
Aqil <aqilll083@gmail.com>
2023-03-05 13:13:50 +0400

    chore: remove `+layout.server.ts`

--- src/routes/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-	const { user, session } = await locals.validateUser();
-	return { user };
-};
