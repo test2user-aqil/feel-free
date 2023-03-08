b448430cb2c94fbf2797d35092a37c1b99783329
Aqil <aqilll083@gmail.com>
2023-03-08 19:39:14 +0400

    feat: make article page

--- src/routes/(app)/a/[articleId]/+page.svelte
+++ src/routes/(app)/a/[articleId]/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import SvelteMarkdown from 'svelte-markdown';
+	import { formatRelative } from 'date-fns';
 
 	export let data: PageData;
 	$: ({ article } = data);
@@ -10,7 +12,20 @@
 	<meta name="description" content={article.description} />
 </svelte:head>
 
-<h3>{article.title}</h3>
-<h4>{data.user?.name}</h4>
-<p>{article.content}</p>
-<small>{article.date}</small>
+<div class="flex flex-col gap-3 p-3">
+	<h1 class="font-semibold text-3xl text-center mt-3 py-3">{article.title}</h1>
+
+	<div class="markdown-body bg-black p-7 rounded-lg shadow-lg shadow-black">
+		<SvelteMarkdown source={article.content} />
+	</div>
+
+	<div class="flex justify-between items-center px-2">
+		<h4 class="link font-semibold">
+			<a href={`/u/${data.user?.username}`}>{data.user?.name}</a>
+		</h4>
+		<small
+			>{formatRelative(article.date, new Date()).charAt(0).toUpperCase() +
+				formatRelative(article.date, new Date()).slice(1)}</small
+		>
+	</div>
+</div>
