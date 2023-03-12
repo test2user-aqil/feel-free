eb1f08e6dfc5af29c34bec1b9e29ab1d6d79e8aa
Aqil <aqilll083@gmail.com>
2023-03-12 12:00:21 +0400

    feat: implement form validation to `/new`

--- src/routes/(auth)/new/+page.server.ts
+++ src/routes/(auth)/new/+page.server.ts
@@ -1,6 +1,29 @@
 import type { Actions, PageServerLoad } from './$types';
 import { prisma } from '$lib/server/prisma';
 import { fail, redirect } from '@sveltejs/kit';
+import { z } from 'zod';
+
+const articleSchema = z.object({
+	id: z
+		.string({ required_error: 'Id is required' })
+		.min(6, { message: 'Id must be 6 or more characters long' })
+		.max(64, { message: 'Id must be 64 or fewer characters long' })
+		.toLowerCase()
+		.trim(),
+	title: z
+		.string({ required_error: 'Title is required' })
+		.min(6, { message: 'Title must be 6 or more characters long' })
+		.max(64, { message: 'Title must be 64 or fewer characters long' })
+		.trim(),
+	content: z
+		.string({ required_error: 'Content is required' })
+		.min(12, { message: 'Content must be 12 or more characters long' })
+		.max(4047, { message: 'Content must be 4047 or fewer characters long' }),
+	description: z
+		.string()
+		.max(127, { message: 'Description must be 127 or fewer characters long' })
+		.trim()
+});
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.validate();
@@ -16,9 +39,18 @@ export const actions: Actions = {
 			throw redirect(302, '/');
 		}
 
-		const { id, title, content, description } = Object.fromEntries(
-			await request.formData()
-		) as Record<string, string>;
+		const formData = Object.fromEntries(await request.formData()) as Record<string, string>;
+		const { id, title, content, description } = formData;
+
+		try {
+			articleSchema.parse(formData);
+		} catch (err: any) {
+			const { fieldErrors: errors } = err.flatten();
+			return {
+				data: formData,
+				errors
+			};
+		}
 
 		try {
 			await prisma.article.create({
--- src/routes/(auth)/new/+page.svelte
+++ src/routes/(auth)/new/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+	import type { ActionData } from './$types';
+	export let form: ActionData;
+</script>
+
 <svelte:head>
 	<title>New Article &bull; Feel Free</title>
 </svelte:head>
@@ -6,31 +11,75 @@
 	action="?/createArticle"
 	method="POST"
 	autocomplete="off"
-	class="flex flex-col md:px-5 px-3 gap-5"
+	class="flex flex-col md:px-5 px-3 py-5 gap-5 article-form"
 >
 	<h3 class="font-semibold text-2xl text-center mt-4">New Article</h3>
 
 	<div class="flex flex-col gap-3">
+		<!-- Error Messages -->
+		{#if form?.errors?.id}
+			<span class="err-msg">{form?.errors?.id[0]}</span>
+		{/if}
+		{#if form?.errors?.title}
+			<span class="err-msg">{form?.errors?.title[0]}</span>
+		{/if}
+		{#if form?.errors?.description}
+			<span class="err-msg">{form?.errors?.description[0]}</span>
+		{/if}
+		{#if form?.errors?.content}
+			<span class="err-msg">{form?.errors?.content[0]}</span>
+		{/if}
+
+		<!-- Inputs -->
 		<div class="grid md:grid-cols-2 gap-3">
+			<!-- Id Input -->
 			<div class="flex md:items-center flex-col md:flex-row md:gap-3 gap-1">
 				<label for="id"> Id </label>
-				<input type="text" id="id" name="id" class="input grow" />
+				<input type="text" id="id" name="id" class="input grow" value={form?.data?.id ?? ''} />
 			</div>
+
+			<!-- Title Input -->
 			<div class="flex md:items-center flex-col md:flex-row md:gap-3 gap-1">
 				<label for="title"> Title </label>
-				<input type="text" id="title" name="title" class="input grow" />
+				<input
+					type="text"
+					id="title"
+					name="title"
+					class="input grow"
+					value={form?.data?.title ?? ''}
+				/>
 			</div>
 		</div>
+
+		<!-- Description Input -->
 		<div class="flex items-stretch flex-col gap-1">
 			<label for="description"> Description </label>
-			<input type="text" id="description" name="description" class="input" />
+			<input
+				type="text"
+				id="description"
+				name="description"
+				class="input"
+				value={form?.data?.description ?? ''}
+			/>
 		</div>
-		<div class="flex items-stretch flex-col gap-1">
+
+		<!-- Content Input -->
+		<div class="flex items-stretch flex-col gap-1 relative">
 			<label for="content" class="mt-1"> Content </label>
-			<textarea id="content" name="content" rows={5} class="input w-full" />
+			<textarea
+				id="content"
+				name="content"
+				rows={7}
+				class="input w-full font-mono"
+				value={form?.data?.content ?? ''}
+			/>
+			<span class="absolute bottom-1 right-4 font-mono font-bold text-xs text-white/30 select-none"
+				>MD Supported</span
+			>
 		</div>
 	</div>
 
+	<!-- Submit Buttons -->
 	<div class="flex justify-between items-center gap-2">
 		<a href="/" class="btn-danger-bordered sm:flex-1 text-center">Cancel</a>
 		<button type="submit" class="btn-primary sm:flex-1">Publish Article</button>
